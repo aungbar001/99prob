45ef856907c5ae7aa93343b3197ca953229288f6
Aungelia Barletta <aungieb0977@gmail.com>
2020-02-14 06:04:23 +0000

    Initial commit

---
+++ 99prob.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main () {
+  fmt.Println("you will choose a number between 10 and 49 for this example we will use 16 now you will take that  16")
+  fmt.Println(99-16)
+  fmt.Println("Then we will ask the other person to to choose a number bewteen 50 and 99, this person choose 24 ")
+  fmt.Println("after that take the number given in this case 24 and add that to you answer in this case it is 83")
+  fmt.Println(24+83)
+  fmt.Println("now take out the 100 and add 1 to the remnder in this case it would be 8")
+  fmt.Println(107-100+1)
+  fmt.Println("alright now that is done we then take the number we have witch is 8, and subtract that form the orignial number witch in this case is 16")
+  fmt.Println(16-8)
+}
